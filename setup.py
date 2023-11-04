c1d65cc51a1ade314200b21908fa70f94a91dc7c
JosuEliz <115068172+JosuEliz@users.noreply.github.com>
2023-11-04 13:23:14 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
    author='Josu Elizburu',
    author_email='jelizburu001@ikasle.ehu.eus',
    packages=['PaqueteJosuEliz', 'PaqueteJosuEliz.test'],
-   url='GITHUB-eko linka',
+   url='https://github.com/JosuEliz/JosuEliz_PaquetePython',
    license='LICENSE.txt',
    description='Este paquete es un paquete para complementar la entrega basica de Software Matematico y Estadistico',
    long_description=open('README.txt').read(),
